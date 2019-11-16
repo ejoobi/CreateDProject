f1346705e79ffa1770dad7a57cd6a274d5bc7048
LeshotoDinao <49591514+LeshotoDinao@users.noreply.github.com>
2019-11-16 16:00:49 +0200

    Edited the configure file

--- config.py
+++ config.py
@@ -11,9 +11,9 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_ID = os.environ.get("MicrosoftAppId", "282f8b1e-9e5f-4c69-af56-369eef9234e7")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "rZ4GwGYGnX/WvBeZ5]7.V-CBX4hm/4FZ")
     LUIS_APP_ID = os.environ.get("LuisAppId", "8445d1de-35a1-4389-8b9c-39033583b434")
     LUIS_API_KEY = os.environ.get("LuisAPIKey", "d7a45a9a625b49dd9c69be435aef1431")
     # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
-    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "https://westus.api.cognitive.microsoft.com/luis/api/v2.0")
+    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "https://westus.api.cognitive.microsoft.com")
